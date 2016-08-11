cb646da97ca6cc71965fef2dd4253a01b93368cf
moshe malka <moshemalka2014@gmail.com>
2016-08-11 06:07:38 +0300

    Delete EmailConfig.java

--- java/com/example/myapplication/EmailConfig.java
+++
@@ -1,9 +0,0 @@
-package com.example.myapplication;
-
-/**
- * Created by Moshe Malka on 15/05/2016.
- */
-public class EmailConfig {
-    public static final String EMAIL ="moshemalka2014@gmail.com";
-    public static final String PASSWORD = "sdsddfigkbjvxqoz";
-}
